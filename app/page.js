321cab77d47c387f8c8e6abcca5e652de8c590c5
aquaticcalf <aquaticcalf@proton.me>
2024-01-28 11:17:01 +0530

    meow

--- app/page.js
+++ app/page.js
@@ -1,13 +1,14 @@
 import Image from "next/image";
+import 'https://cdn.tailwindcss.com'
 
 export default function Home() {
   return (
-    <div class="bg-black flex min-h-screen">
-        <div class="bg-red-300 grow-[1] p-5 flex flex-col gap-2">
-        <div id="text-block" class="bg-black text-white text-center py-2 codeblock" draggable="true">text</div>
-        <div id="img-block" class="bg-green-300 text-black text-center py-2 codeblock" draggable="true">image</div>
+    <div className="bg-black flex min-h-screen">
+        <div className="bg-red-300 grow-[1] p-5 flex flex-col gap-2">
+        <div id="text-block" className="bg-black text-white text-center py-2 codeblock" draggable="true">text</div>
+        <div id="img-block" className="bg-green-300 text-black text-center py-2 codeblock" draggable="true">image</div>
     </div>
-    <div class="bg-blue-300 grow-[2] p-5 flex flex-col gap-2"></div>
+    <div className="bg-blue-300 grow-[2] p-5 flex flex-col gap-2"></div>
     </div>  
   );
 }
