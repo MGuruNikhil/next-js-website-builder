cf6d18e6651e2ea4bad4a9a9eac99409f8be76a9
sam <sampreethm@proton.me>
2024-01-28 13:31:00 +0530

    meow

--- app/layout.js
+++ app/layout.js
@@ -11,10 +11,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        <link rel="shortcut icon" href="https://avatars.githubusercontent.com/u/149516463?s=88&v=4" type="image/x-icon"/>
-        <script src="https://cdn.tailwindcss.com"></script>
-      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
