50d3a9545e1dff12b7ca5e14c29e84140f324a11
aquaticcalf <aquaticcalf@proton.me>
2024-01-28 11:48:00 +0530

    meow

--- app/toolbar.js
+++ app/toolbar.js
@@ -1,12 +1,30 @@
 import React from 'react'
+import { useDrag } from 'react-dnd'
+
+const ItemTypes = {
+    TEXT: 'text',
+    IMAGE: 'image',
+  }  
+
+const DraggableItem = ({ type }) => {
+    const [, drag] = useDrag(() => ({
+      type,
+      collect: (monitor) => ({
+        isDragging: !!monitor.isDragging(),
+      }),
+    }))
+  
+    return <div ref={drag}>{type}</div>
+  }
+  
 
 const Toolbar = () => {
   return (
 
 <div className="bg-red-300 grow-[1] p-5 flex flex-col gap-2">
-<div id="text-block" className="bg-black text-white text-center py-2 codeblock" draggable="true">text</div>
-<div id="img-block" className="bg-green-300 text-black text-center py-2 codeblock" draggable="true">image</div>
+<DraggableItem type={ItemTypes.TEXT} className="bg-black text-white text-center py-2 codeblock" />
+<DraggableItem type={ItemTypes.IMAGE} className="bg-green-300 text-black text-center py-2 codeblock"/>
 </div>
   )}
 
-  export default Toolbar
+  export { Toolbar, ItemTypes }
